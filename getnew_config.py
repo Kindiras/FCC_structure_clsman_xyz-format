cfb2801d785ae58bf6ff0cb3f653e5b32bb99a07
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2018-11-07 15:00:59 -0500

    Update getnew_config.py

--- getnew_config.py
+++ getnew_config.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     strainx=int(input("what is x-strain? could be 1.04 for tensile 4 per or 0.96 for compressive 4 per or 1 for no strain "))
     strainy=int(input("what is y-strrain?"))
     lattice=float(input("what is the lattice constant?"))
-    lsize=int(input("what is configuration size like 10 for 10xa where a is lattice constant?"))
+    lsize=int(input("what is configuration size like 20 for 10xa where a is lattice constant?"))
     layers=int(input("how many layers you want like 5 for 5 layers?"))
     filename=input("what is the file name lke data.dat for clsman or data.xyz for xyz format?")
     position, itype, ia = atoms_position (strainx,strainy,lsize,lattice,layers)
